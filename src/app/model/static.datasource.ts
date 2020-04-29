2248849631dd5f3342299a1f0df5247a85f79e91
Sadan Iqbal <sadaniqbal@gmail.com>
2020-04-29 03:54:15 -0700

    created order.repository

--- src/app/model/static.datasource.ts
+++ src/app/model/static.datasource.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Product } from "./product.model";
 import { Observable, from } from "rxjs";
+import { Order } from "./order.model";
 
 @Injectable()
 export class StaticDataSource {
