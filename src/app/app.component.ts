5e7ecf887d11ef11e7543b1ee120a28e15316147
Sadan Iqbal <sadaniqbal@gmail.com>
2020-04-29 00:37:05 -0700

    all init files

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app",
+  // templateUrl: "./app.component.html",
+  template: "<store></store>",
+})
+export class AppComponent {
+  title = "IS645Chap7-SuperStore";
+}
