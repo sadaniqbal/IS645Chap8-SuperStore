e9960ea312576a74a318a061d1182c575d9456b2
Sadan Iqbal <sadaniqbal@gmail.com>
2020-04-29 03:57:39 -0700

    Created checkout.component

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -3,6 +3,7 @@ import { ProductRepository } from "./product.repository";
 import { StaticDataSource } from "./static.datasource";
 import { Cart } from "./cart.model";
 import { Order } from "./order.model";
+import { OrderRepository } from "./order.repository";
 
 @NgModule({
   providers: [
